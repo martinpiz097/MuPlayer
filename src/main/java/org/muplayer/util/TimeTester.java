68c006da101ceeeb48896c7becfb572ddee97406
martinpiz097 <martinpiz097@gmail.com>
2024-03-24 13:24:18 -0300

    Se implementa mediciones de tiempo mediante un TimeTester

---
+++ src/main/java/org/muplayer/util/TimeTester.java
@@ -0,0 +1,63 @@
+package org.muplayer.util;
+
+import lombok.extern.java.Log;
+import org.muplayer.properties.log.LogConfig;
+import org.muplayer.properties.log.LogConfigKeys;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+
+@Log
+public class TimeTester {
+    private volatile double startTimestamp;
+    private volatile double finishTimestamp;
+    private final TimeUnit timeUnit;
+
+    public TimeTester(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    private double getCurrentTime() {
+        double currentTime = System.nanoTime();
+
+        switch (timeUnit) {
+            case NANOSECONDS:
+                return currentTime;
+            case MICROSECONDS:
+                return currentTime / Math.pow(1000, 1);
+            case MILLISECONDS:
+                return currentTime / Math.pow(1000, 2);
+            case SECONDS:
+                return currentTime / Math.pow(1000, 3);
+            default:
+                return 0;
+        }
+    }
+
+    public void start() {
+        startTimestamp = getCurrentTime();
+    }
+
+    public void finish() {
+        finishTimestamp = getCurrentTime();
+    }
+
+    public String getTimeDifference() {
+        final double timeDiff = finishTimestamp - startTimestamp;
+        final NumberFormat numberFormat = new DecimalFormat("#0.000");
+        return numberFormat.format(timeDiff)
+                + " " + timeUnit.name();
+    }
+
+    public void logTimeDifference(String msg) {
+        LogConfig logConfig = LogConfig.getInstance();
+        String logLevel = logConfig.getProperty(LogConfigKeys.JAVA_LOG_LEVEL);
+
+        if (!logLevel.equals(Level.OFF.getName())) {
+            System.out.println(msg + ": " + getTimeDifference());
+        }
+    }
+}
