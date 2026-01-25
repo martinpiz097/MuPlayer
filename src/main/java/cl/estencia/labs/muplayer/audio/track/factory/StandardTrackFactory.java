1d7d3fd4b0615d53a30f8bc2ee2534f31ff7526e
Martín Pizarro <martinpiz097@gmail.com>
2026-01-25 14:49:34 -0300

    refactor: simplificacion de logica

--- src/main/java/cl/estencia/labs/muplayer/audio/track/factory/StandardTrackFactory.java
+++ src/main/java/cl/estencia/labs/muplayer/audio/track/factory/StandardTrackFactory.java
@@ -1,22 +1,48 @@
 package cl.estencia.labs.muplayer.audio.track.factory;
 
+import cl.estencia.labs.muplayer.audio.common.enums.SupportedAudioExtensions;
 import cl.estencia.labs.muplayer.audio.track.Track;
+import cl.estencia.labs.muplayer.audio.track.format.FlacTrack;
+import cl.estencia.labs.muplayer.audio.track.format.MP3Track;
+import cl.estencia.labs.muplayer.audio.track.format.OGGTrack;
+import cl.estencia.labs.muplayer.audio.track.format.PCMTrack;
+import cl.estencia.labs.muplayer.audio.util.AudioFileUtil;
 import cl.estencia.labs.muplayer.core.exception.AudioFileInvalidException;
 import cl.estencia.labs.muplayer.core.exception.FormatNotSupportedException;
-import cl.estencia.labs.muplayer.io.file.AudioFileScanner;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
-import java.io.IOException;
+
+import static cl.estencia.labs.muplayer.audio.util.AudioFileUtil.getAudioExtensionFromFormat;
 
 @Slf4j
 public class StandardTrackFactory implements TrackFactory {
 
     @Override
     public Track getTrack(File dataSource) throws FormatNotSupportedException, AudioFileInvalidException {
-        AudioFileScanner audioFileScanner = new AudioFileScanner(dataSource);
-        return audioFileScanner.loadTrack();
+        try {
+            // aca ya se evalua si el formato esta soportado, solo que de una manera mas directa
+            if (dataSource == null || !dataSource.exists() || dataSource.isDirectory()) {
+                throw new AudioFileInvalidException(dataSource);
+            }
+
+            final String fileFormatName = AudioFileUtil.getFileFormatName(dataSource);
+            final SupportedAudioExtensions extension = getAudioExtensionFromFormat(fileFormatName);
+            if (extension == null) {
+                throw new FormatNotSupportedException(fileFormatName);
+            }
+
+            return switch (extension) {
+                case aifc, aiff, au, snd, wav -> new PCMTrack(dataSource);
+                case flac -> new FlacTrack(dataSource);
+                case mp3 -> new MP3Track(dataSource);
+                case ogg -> new OGGTrack(dataSource);
+            };
+        } catch (IllegalArgumentException e) {
+//            log.error(e.getMessage(), e);
+            log.warn(e.getMessage());
+            return null;
+        }
     }
+
 }
